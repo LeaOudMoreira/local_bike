ba0a1194ebdaa46f6d80c0f930890bd9fffba907
Léa Oudart <3a.oudart@gmail.com>
2026-04-04 13:15:27 +0000

    add: intermediate table w/ detail orders

---
+++ models/intermediate/int_sales__order_item_detail.sql
@@ -0,0 +1,15 @@
+SELECT
+oi.order_id,
+o.store_id,
+staff_id,
+customer_id,
+product_id,
+quantity,
+list_price,
+discount,
+(quantity*list_price)*(1-discount) AS total_amount_item
+FROM {{ref('stg_local_bike__order_item')}} oi
+LEFT JOIN  {{ref('stg_local_bike__orders')}} o ON oi.order_id = o.order_id
+
+
+
