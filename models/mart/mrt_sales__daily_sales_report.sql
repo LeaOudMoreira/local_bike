0c708aec9c2de73afc36bff644f612430bc5067f
Léa Oudart <3a.oudart@gmail.com>
2026-04-06 17:40:19 +0000

    fix: issue date fonction

--- models/mart/mrt_sales__daily_sales_report.sql
+++ models/mart/mrt_sales__daily_sales_report.sql
@@ -1,11 +1,12 @@
 SELECT
 order_date,
-year(order_date) as year_order,
-month(order_date) AS month_order,
+date_trunc(order_date,year) as year_order,
+date_trunc(order_date,month) AS month_order,
 store_name,
 product_name,
 sum(quantity) as quantity_order_item,
 sum(total_amount_item) AS total_revenue_item
-FROM {{ ref('int_sales__order_item_detail') }}
+FROM {{ ref('int_sales__order_item_detail') }} oid
+LEFT JOIN {{ ref('int_production__product') }} p on p.product_id = oid.product_id
 group by order_date,year_order, month_order, store_name,
 product_name
