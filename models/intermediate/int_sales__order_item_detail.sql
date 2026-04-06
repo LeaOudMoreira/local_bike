160163e528e280df0f977db04ef95b17a5e68c78
Léa Oudart <3a.oudart@gmail.com>
2026-04-06 16:54:28 +0000

    Enrich mart model w/ new table and fix problems on intermediate table

--- models/intermediate/int_sales__order_item_detail.sql
+++ models/intermediate/int_sales__order_item_detail.sql
@@ -1,7 +1,9 @@
 SELECT
 oi.order_id,
 o.store_id,
-staff_id,
+store.store_name,
+o.staff_id,
+concat(first_name," ",last_name) AS staff_name,
 customer_id,
 product_id,
 quantity,
@@ -10,7 +12,5 @@ discount,
 (quantity*list_price)*(1-discount) AS total_amount_item
 FROM {{ref('stg_local_bike__order_item')}} oi
 LEFT JOIN  {{ref('stg_local_bike__orders')}} o ON oi.order_id = o.order_id
-
-
-
-
+LEFT JOIN {{ ref('stg_local_bike__staffs') }} s on s.staff_id = o.staff_id
+LEFT JOIN {{ ref('stg_local_bike__stores') }} store ON store.store_id = o.store_id
