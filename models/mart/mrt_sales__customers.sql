cc7ca14acb25eb366519ad34aea11d512a5331cf
Léa Oudart <3a.oudart@gmail.com>
2026-04-07 12:30:13 +0000

    add customer table

--- models/mart/mrt_sales__customers.sql
+++ models/mart/mrt_sales__customers.sql
@@ -3,4 +3,4 @@ customer_id,
 store_id,
 count(distinct order_id) AS nb_order,
 sum(total_amount_item) AS revenue
-FROM {{ ref('int_sales__order_item_detail') }}
+FROM {{ ref('int_sales__order_item_detail') }} oid
